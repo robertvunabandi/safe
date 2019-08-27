723a891e9ee5612178cd464af222dd9262d76993
robertvunabandi <rvunabandi@gmail.com>
2019-08-27 19:16:47 -0400

    returning exit code for file missing

--- safe/core/cmd_convert.py
+++ safe/core/cmd_convert.py
@@ -3,10 +3,11 @@ from safe.common import exit_codes
 from safe.core.converter import Converter
 
 
-def run(converter: Converter, filepath: pathlib.Path, decrypt: bool) -> None:
+def run(converter: Converter, filepath: pathlib.Path, decrypt: bool) -> int:
     # todo: write a function for this. should think about how
     #  this would fit with vim and other commands
     if not filepath.exists():
-        # todo - better handle this with exit codes
-        raise ValueError("File doesn't exist") 
+        # todo - better handle this with error messages,
+        #  like a RunResult thing
+        return exit_codes.FILE_DOES_NOT_EXIST
     raise NotImplementedError
